dabac062af25b5a1e308cb401b42958cf84ef93e
shubham <shubhamkjais40@gmai.com>
2024-02-23 00:53:46 +0530

    extracts runner dev

--- src/extract.py
+++ src/extract.py
@@ -1,12 +1,13 @@
 import requests
 import pandas as pd
 import os
-
+from pathlib import Path
 
 class PMKisan_Extracts:
-    def __init__(self):
+    def __init__(self,output_dir='EXTRACTS_RAW'):
         self.HEADERS = {'User-Agent': 'ReqBin Python Client/1.0'}
-        self.URL="https://dackkms.gov.in/Account/API/kKMS_QueryData.aspx?StateCD=01&DistrictCd=0104"
+        self.URL = "https://dackkms.gov.in/Account/API/kKMS_QueryData.aspx?StateCD=01&DistrictCd=0104"
+        self.OUTPUTPATH = Path.cwd().parent.joinpath(output_dir)
     
     def test_api_conn(self,month,year):
         payload={'Month':f'{month}','Year':f'{year}'}
@@ -23,11 +24,15 @@ class PMKisan_Extracts:
         datatable.to_excel(filepath+".xlsx",engine='openpyxl',index=False)
         return "File saved successfully"
 
+    def kisan_monthly_extracts(self,month,year):
+        data = self.extract_monthly_data(month,year)
+        self.saveMonthTableToxls(data,str(self.OUTPUTPATH)+"//"+f'{year}'+'_'+f'{month}'+"extracts")
+        return 0
 
     def kisan_backlog_extracts(self,startMonth,endMonth,year):
         for month in range(startMonth,endMonth+1):
             data=self.extract_monthly_data(month,year)
-            self.saveMonthTableToxls(data,".//extracts//"+f'{year}'+'_'+f'{month}'+"extracts")
+            self.saveMonthTableToxls(data,str(self.OUTPUTPATH)+"//"+f'{year}'+'_'+f'{month}'+"extracts")
         return 0
 
     def clear_extractsDir():
--- src/run.py
+++ src/run.py
@@ -1,9 +1,59 @@
-import datetime as dt
 from extract import PMKisan_Extracts
+import datetime as dt
+import argparse
+#import logging
 
-ke=PMKisan_Extracts()
+#Modifying Logging configuration
+#logging.basicConfig(filename='extracts_runner.log',level=logging.DEBUG)
 
-ke.kisan_backlog_extracts(6,12,2023)
+parser = argparse.ArgumentParser(description = 'Take parameter as type of extracts needed and object params')
+parser.add_argument('extracts_type',type=str,help='Adhoc OR Monthly')
+parser.add_argument('-m','--month',type=int,help="Enter valid month like:'MM' of year")
+parser.add_argument('-y','--year',type=int,help="Enter valid year like:'YYYY'")
+parser.add_argument('-sm','--startmonth',type=int,help="Enter valid start month like:'MM' of year")
+parser.add_argument('-em','--endmonth',type=int,help="Enter valid end month like:'MM' of year")
+parser.add_argument('-out','--outputdir',default='TESTDIR',type=str,help="Enter valid directory Name to ingesting Extracts")
 
-#df=ke.extract_monthly_data(10,2023)
-#ke.save_tableToXlsFile(df,)
+#logging.DEBUG("Waiting for Extracts Type to perform extracts generation")
+args = parser.parse_args()
+EXTRACTS_TYPE = args.extracts_type.upper()
+VALID_EXTRACTS_INPUT = ['ADHOC','MONTHLY']
+
+#logging.debug("Validating Extracts as per Input")
+if EXTRACTS_TYPE in VALID_EXTRACTS_INPUT:
+    print(f"Extracts_Type -> {EXTRACTS_TYPE}")
+else:
+    print("Please enter valid Extracts Type from options-> [ADHOC | MONTHLY]")
+
+
+# Conditional Run for input reuqirements
+
+if EXTRACTS_TYPE == 'MONTHLY':
+    print("Monthly Extracts will run")
+    MONTH = args.month
+    YEAR = args.year
+    OUTDIR = args.outputdir.upper()
+    print(f"Pulling DataSet for params {MONTH},{YEAR},{OUTDIR} job..")
+    monObj = PMKisan_Extracts(OUTDIR)
+    monObj.kisan_monthly_extracts(MONTH,YEAR)
+    print(f"Successfully ingested Extracts for {YEAR}-{MONTH} in {OUTDIR}!!")
+
+if EXTRACTS_TYPE == 'ADHOC':
+    print("Adhoc Extracts will run")
+    ST_MONTH=args.startmonth
+    EN_MONTH = args.endmonth
+    YEAR_ADHOC = args.year
+    OUTDIR = args.outputdir.upper()
+    print(f"{ST_MONTH},{EN_MONTH},{YEAR_ADHOC},{OUTDIR}")
+    adhocobj=PMKisan_Extracts(OUTDIR)
+    print(adhocobj.OUTPUTPATH)
+    adhocobj.kisan_backlog_extracts(ST_MONTH,EN_MONTH,YEAR_ADHOC)
+    print(f"Successfully ingested ADHOC Extracts for Year:{YEAR_ADHOC}-Month:{ST_MONTH} to Month:{EN_MONTH} in {OUTDIR}!!")
+
+#ke=PMKisan_Extracts("EXTRACTS_RAW")
+
+# #ke.kisan_backlog_extracts(6,12,2023)
+# print(ke.test_api_conn(1,2023))
+# ke.kisan_monthly_extracts(1,2024)
+# #ke.save_tableToXlsFile(df,)
+
